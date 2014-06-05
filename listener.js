4a92ad66fa15cd64557ed7520b2d579c0f0d5a49
darkrider <mormehtar@gmail.com>
2014-06-05 12:05:48 +0400

    last version

--- listener.js
+++ listener.js
@@ -7,44 +7,87 @@ var amqp = require("amqplib/callback_api");
 var common_options = {durable: true};
 var nickname = "darkrider";
 
-var global_queue = nickname + "_global2";
-var private_queue = nickname + "_private2";
-var private_route = "direct." + nickname;
-var global_route = "main.chat";
+var readline = require('readline');
 
-var connection = amqp.connect("amqp://fe-01.pharmhub.ru", function(err, conn) {
-    conn.createChannel(function(err, channel) {
-        channel.assertQueue(global_queue, common_options, function(err, ok) {
-            channel.assertExchange("chat3", "direct", common_options, function(err, ok) {
-                channel.bindQueue(global_queue, "chat3", global_route, common_options);
-                channel.consume(global_queue, function(msg) {
-//                    console.log(global_route);
-//                    console.log(msg.fields);
-                    console.log(msg.content.toString());
-                    channel.ack(msg);
-                }, common_options);
-            });
-        });
-    });
-    conn.createChannel(function(err, channel) {
-        channel.assertQueue(private_queue, common_options, function(err, ok) {
-            channel.assertExchange("chat3", "direct", common_options, function(err, ok) {
-                channel.bindQueue(private_queue, "chat3", private_route, common_options);
-                channel.consume(private_queue, function(msg) {
-//                    console.log(private_route);
-//                    console.log(msg.fields);
-                    console.log("Private:", msg.content.toString());
-                    channel.ack(msg);
-                }, common_options);
-            });
-        });
-    });
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
 
-//connection.then(function (connection) {
-//    process.once('SIGINT', connection.close.bind(connection));
-//    var channel = connection.createChannel();
-//    channel.then(channel.assertQueue("main", common_options);
-//    channel.then()
-//    return channel;
-//});
+var private_route = "*." + nickname;
+var global_route = "chat.*";
+var last_sender = "";
+
+function parse(msg, verb){
+    try {
+        var params = JSON.parse(msg.content.toString());
+        params.dt = new Date(params.dt);
+        if (params.sender != nickname) {
+            last_sender = params.sender;
+        }
+        var message = params.dt.getHours() + ":" + params.dt.getMinutes() + " " + params.sender + " " + verb + ": " + params.message;
+    }
+    catch (e){
+        message = "Error in " + verb + ": " + msg.content.toString();
+    }
+    return message;
+}
+
+amqp.connect("amqp://fe-01.pharmhub.ru", function(err, conn) {
+    conn.createChannel(function(err, channel) {
+        channel.assertQueue("", {exclusive:true}, function(err, ok) {
+            var global_queue = ok.queue;
+            channel.assertExchange("chat4", "topic", common_options, function(err, ok) {
+                channel.bindQueue(global_queue, "chat4", global_route);
+                channel.consume(global_queue, function(msg) {
+                    console.log(parse(msg, "said"));
+                    channel.ack(msg);
+                }, common_options);
+            });
+        });
+    });
+    conn.createChannel(function(err, channel) {
+        channel.assertQueue("", {exclusive:true}, function(err, ok) {
+            var private_queue = ok.queue;
+            channel.assertExchange("chat4", "topic", common_options, function(err, ok) {
+                channel.bindQueue(private_queue, "chat4", private_route);
+                channel.consume(private_queue, function(msg) {
+                    console.log(parse(msg, "whispered"));
+                    channel.ack(msg);
+                }, common_options);
+            });
+        });
+    });
+
+    conn.createChannel(function(err, channel) {
+        channel.assertExchange("chat4", "topic", common_options, function(err, ok) {
+            rl.on('line', function (cmd) {
+                if (cmd[0] == "-"){
+                    var space = cmd.indexOf(" ");
+                    var target = cmd.substr(1, space - 1).trim();
+                    if (target = "-"){
+                        if (last_sender){
+                            target = last_sender;
+                        } else {
+                            target = nickname;
+                        }
+                    }
+                    var msg = cmd.substr(space).trim();
+
+                    channel.publish("chat4", "direct." + target, new Buffer(JSON.stringify({
+                        dt: new Date(),
+                        message: msg,
+                        sender: nickname
+                    })), common_options);
+                } else {
+                    channel.publish("chat4", "chat.main", new Buffer(JSON.stringify({
+                        dt: new Date(),
+                        message: cmd,
+                        sender: nickname
+                    })), common_options);
+                }
+
+            });
+        })
+    });
+});
